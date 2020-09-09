acc957f3dde6830b0f52c9ae93a7a4b068819bdb
mroldan747 <mroldan747@gmail.com>
2020-09-09 22:08:18 +0200

    init repository

---
+++ src/main/java/com/listInApp/listInApp/ListInAppApplication.java
@@ -0,0 +1,13 @@
+package com.listInApp.listInApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListInAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListInAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listInApp/listInApp/ListInAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listInApp.listInApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListInAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
